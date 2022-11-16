df0ce18e8f05818f6072808d2f9a6852172a9531
eslam <eslammongy995@gmail.com>
2022-11-16 16:00:00 +0200

    adding extra functions

--- bin/binary__search.dart
+++ bin/binary__search.dart
@@ -1,10 +1,12 @@
+// ignore_for_file: unused_import
+
 import 'package:binary__search/binarysearch.dart';
 
 void main(List<String> arguments) {
   var binarySearch = BinarySearch();
   var arrayOfNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  var nums = [4, 5, 6, 7, 0, 1, 2];
   // ignore: unused_local_variable
   var arrayOfNames = ['eslam', 'nada', 'samer', 'mongy', 'hamza'];
-  print(
-      'The Index Of Your Value Is: ${binarySearch.doBinarySearchNums(arrayOfNums, 5)}!');
+  print('The Index Of Your Value Is: ${binarySearch.mySqrt(8)}!');
 }
--- lib/binarysearch.dart
+++ lib/binarysearch.dart
@@ -1,15 +1,20 @@
 class BinarySearch {
   // binary search with looping
-  doBinarySearchNums(List<int> array, int inputValue) {
+  doBinarySearchNums(List<int> array, int target) {
+    if (array.isEmpty) {
+      return -1;
+    }
+
     int left = 0;
     int right = array.length - 1;
+
     while (left <= right) {
       var middle = left + (right - left) ~/ 2;
       var gusse = array[middle];
 
-      if (gusse == inputValue) {
+      if (gusse == target) {
         return middle;
-      } else if (gusse > inputValue) {
+      } else if (gusse > target) {
         right = middle - 1;
       } else {
         left = middle + 1;
@@ -19,12 +24,12 @@ class BinarySearch {
   }
 
   // binary search with looping
-  doBinarySearchString(List<String> array, String inputValue) {
+  doBinarySearchString(List<String> array, String target) {
     int left = 0;
     int right = array.length - 1;
     while (left <= right) {
       var middle = left + (right - left) ~/ 2;
-      var gusse = array[middle].compareTo(inputValue);
+      var gusse = array[middle].compareTo(target);
 
       if (gusse == 0) {
         return middle;
@@ -39,20 +44,43 @@ class BinarySearch {
 
   // binary search with recurstion
   recurstionBinarySearch(
-      List<dynamic> array, int start, int end, dynamic inputValue) {
+      List<dynamic> array, int start, int end, dynamic target) {
     if (start > end) {
       return -1;
     }
     var middle = start + ((end - start) ~/ 2);
     var gusse = array[middle];
-    if (gusse == inputValue) {
+    if (gusse == target) {
       return middle;
-    } else if (middle > array.indexOf(inputValue)) {
+    } else if (middle > array.indexOf(target)) {
       end = middle - 1;
-      return recurstionBinarySearch(array, start, end, inputValue);
+      return recurstionBinarySearch(array, start, end, target);
     } else {
       start = middle + 1;
-      return recurstionBinarySearch(array, start, end, inputValue);
+      return recurstionBinarySearch(array, start, end, target);
     }
+  }
+
+  int mySqrt(int x) {
+    if (x == 0 || x == 1) {
+      return x;
+    }
+    var start = 0;
+    var end = x ~/ 2;
+    late int ans;
+
+    while (start <= end) {
+      var mid = (start + end) ~/ 2;
+
+      if (mid * mid == x) {
+        return mid;
+      } else if (mid * mid < x) {
+        start = mid + 1;
+        ans = mid;
+      } else {
+        end = mid - 1;
+      }
+    }
+    return ans;
   }
 }
